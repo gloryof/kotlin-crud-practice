0f0c32cd402158f3779d3cb00d3094e5bbcaedd5
Junki Yamada <geek.junkey@gmail.com>
2019-05-16 23:04:19 +0900

    JREのバージョンを12に設定

--- kotlin-crud-practice/build.gradle.kts
+++ kotlin-crud-practice/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "jp.glory"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_12
 
 repositories {
 	mavenCentral()
